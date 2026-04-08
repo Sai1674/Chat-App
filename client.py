f964d86674ef0961df1950383880313252376961
sainatha <144334356+Sai1674@users.noreply.github.com>
2026-04-08 11:13:50 +0530

    Intial commit - Chat-App

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+            print(message)
+        except:
+            print("Disconnected")
+            client.close()
+            break
+
+def write():
+    while True:
+        msg = input()
+        client.send(msg.encode())
+
+threading.Thread(target=receive).start()
+threading.Thread(target=write).start()
